0cd1277e099597dd06335f85fcf35c48399288cc
tupiznak <akej-vonavi@mail.ru>
2016-09-30 02:51:24 +0300

    add signin, login and all api

--- src/main/java/ru/steamtanks/main/RegistrationController.java
+++ src/main/java/ru/steamtanks/main/RegistrationController.java
@@ -8,58 +8,131 @@ import org.springframework.web.bind.annotation.*;
 import ru.steamtanks.models.UserProfile;
 import ru.steamtanks.services.AccauntService;
 
+import javax.servlet.http.HttpSession;
+import java.util.Objects;
+
 /**
  * Created by jake on 24.09.16.
  */
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class RegistrationController {
 
     final private AccauntService accauntService;
+    final private HttpSession httpSession;
 
     @Autowired
-    public RegistrationController(AccauntService accauntService) {
+    public RegistrationController(AccauntService accauntService,
+                                  HttpSession httpSession) {
         this.accauntService = accauntService;
+        this.httpSession = httpSession;
     }
 
-    @RequestMapping(path = "/api/user/add", method = RequestMethod.POST)
-    @CrossOrigin()
+    //user
+    @RequestMapping(path = "/api/user", method = RequestMethod.POST)
     public ResponseEntity userAdd(@RequestBody RegistrationRequest body) {
         String login = body.getLogin();
         String email = body.getEmail();
         String password = body.getPassword();
 
-        if (StringUtils.isEmpty(login)
-                || StringUtils.isEmpty(email)
-                || StringUtils.isEmpty(password)
-                ) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("{}");
-        }
+        //validation on front
+
         final UserProfile existingUser = accauntService.getUser(login);
-        if (existingUser!=null)
-            return ResponseEntity.status(HttpStatus.OK).body("{\"error\":\"1\"}");
+        if (existingUser != null)
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("{}");
 
         accauntService.addUser(login, password, email);
 
+        httpSession.setAttribute("username", login);
+        System.out.println(httpSession.getAttribute("username"));
+        System.out.println(httpSession.getId());
 
-        return ResponseEntity.status(HttpStatus.OK).body("{\"login\":\""+login+"\"}");
-        //return ResponseEntity.ok(new SuccessResponse(login));
-
+        return ResponseEntity.status(HttpStatus.OK).body("{}");
     }
 
+    @RequestMapping(path = "/api/user", method = RequestMethod.DELETE)
+    public ResponseEntity userDel() {
+        String login = (String) httpSession.getAttribute("username");
+        System.out.println(httpSession.getAttribute("username"));
+        System.out.println(httpSession.getId());
 
-    @RequestMapping(path = "/api/user/del", method = RequestMethod.POST)
-    @CrossOrigin()
-    public ResponseEntity userDel(@RequestBody RegistrationRequest body) {
-        String login = body.getLogin();
-
-        final UserProfile existingUser = accauntService.getUser(login);
-        if (existingUser==null)
+        if (StringUtils.isEmpty(login))
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("{}");
 
         accauntService.delUser(login);
+        sessionDel();
 
-        return ResponseEntity.status(HttpStatus.OK).body("{\"status\":\"ok\"}");
+        return ResponseEntity.status(HttpStatus.OK).body("{}");
+    }
+
+    @RequestMapping(path = "/api/user", method = RequestMethod.PUT)
+    public ResponseEntity userCange(@RequestBody RegistrationRequest body) {
+        String login = body.getLogin();
+        String email = body.getEmail();
+        String password = body.getPassword();
+
+        //validation on front
+
+        final UserProfile existingUser = accauntService.getUser(login);
+        if (existingUser == null)
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("{}");
+
+        existingUser.setLogin(login);
+        existingUser.setEmail(email);
+        existingUser.setPassword(password);
+
+        return ResponseEntity.status(HttpStatus.OK).body("{}");
+    }
+
+    //session
+    @RequestMapping(path = "/api/session", method = RequestMethod.GET)
+    public ResponseEntity sessionGet() {
+
+        System.out.println(httpSession.getAttribute("username"));
+        System.out.println(httpSession.getId());
+
+        final UserProfile existingUser = accauntService.getUser((String) httpSession.getAttribute("username"));
+        if (existingUser==null)
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("{}");
+
+        return ResponseEntity.ok().body(
+                "{\"login\":\""+existingUser.getLogin()+"\"," +
+                        "\"email\":\""+ existingUser.getEmail()+"\"}"
+        );
+    }
+
+    @RequestMapping(path = "/api/session", method = RequestMethod.POST)
+    public ResponseEntity sessionLogin(@RequestBody RegistrationRequest body) {
+        String login = body.getLogin();
+        String password = body.getPassword();
+
+        //validation on front
+
+        final UserProfile existingUser = accauntService.getUser(login);
+        if (existingUser == null)
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("{}");
+
+        if (!Objects.equals(existingUser.getPassword(), password))
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("{}");
+
+        httpSession.setAttribute("username", login);
+
+        return ResponseEntity.status(HttpStatus.OK).body(
+                "{\"email\":\""+ existingUser.getEmail()+"\"}"
+        );
+    }
+
+    @RequestMapping(path = "/api/session", method = RequestMethod.DELETE)
+    public ResponseEntity sessionDel() {
+        String login = (String) httpSession.getAttribute("username");
+
+        if (StringUtils.isEmpty(login))
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("{}");
+
+        httpSession.invalidate();
+
+        return ResponseEntity.status(HttpStatus.OK).body("{}");
     }
 
     private static final class RegistrationRequest {
--- src/main/java/ru/steamtanks/models/UserProfile.java
+++ src/main/java/ru/steamtanks/models/UserProfile.java
@@ -1,21 +1,37 @@
 package ru.steamtanks.models;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 /**
  * Created by jake on 24.09.16.
  */
 public class UserProfile {
+    private static final AtomicLong ID_GENETATOR = new AtomicLong(0);
+    private Long id;
     private String login;
     private String email;
     private String password;
-    private int countLogin = 0;
 
     public UserProfile(String login, String password, String email) {
         this.login = login;
         this.email = email;
         this.password = password;
+        this.id = ID_GENETATOR.getAndIncrement();
     }
 
-    public int getCountLogin() {return countLogin;}
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Long getId() { return id; }
 
     public String getLogin() {
         return login;
